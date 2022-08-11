0ee959970836b5c06a4ff54aeb59d049cdad787d
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2022-08-11 10:19:12 -0500

    Update main.tf

--- 00-env-setup/main.tf
+++ 00-env-setup/main.tf
@@ -709,7 +709,7 @@ resource "null_resource" "scoring_notebook_customization" {
 
 resource "null_resource" "vai_pipeline_customization" {
     provisioner "local-exec" {
-        command = "cp ../04-templates/customer_churn_vai_pipeline_template.json ../05-pipelines/ && sed -i s/YOUR_PROJECT_NBR/${local.project_nbr}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_PROJECT_ID/${local.project_id}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_GCP_LOCATION/${local.location}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_EXECUTION_ID/12345678/g ../05-pipelines/customer_churn_vai_pipeline_template.json"
+        command = "mkdir ../05-pipelines && cp ../04-templates/customer_churn_vai_pipeline_template.json ../05-pipelines/ && sed -i s/YOUR_PROJECT_NBR/${local.project_nbr}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_PROJECT_ID/${local.project_id}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_GCP_LOCATION/${local.location}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_EXECUTION_ID/12345678/g ../05-pipelines/customer_churn_vai_pipeline_template.json"
         interpreter = ["bash", "-c"]
     }
 }
